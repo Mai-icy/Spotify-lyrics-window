6fbfc1fa52d9776db552efa6de37cb1ec7e9b48d
Maiicy <Maiicy@foxmail.com>
2026-09-10 17:06:26 +0800

    :bug: fix macOS crash when downloading lyrics

--- SpotifyLyricWindow/components/dialog/lyrics_download_dialog.py
+++ SpotifyLyricWindow/components/dialog/lyrics_download_dialog.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 # -*- coding:utf-8 -*-
-import io
+from functools import partial
+from itertools import zip_longest
 
 from PyQt6.QtCore import *
 from PyQt6.QtGui import *
@@ -8,20 +9,41 @@ from PyQt6.QtWidgets import *
 
 from components.raw_ui import Ui_LyricsDownloadDialog
 from components.dialog import WarningDialog
-from components.work_thread import thread_drive
+from components.work_thread import thread_manager
 from common.api.lyric_api import CloudMusicWebApi, KugouApi, SpotifyApi
 from common.api.exceptions import NoneResultError, NetworkError, UserError
 from common.temp_manage import TempFileManage
+from common.logger import get_logger
+
+
+logger = get_logger(__name__)
+
+
+class _DownloadTask(QThread):
+    """Only plain data/API calls cross into this thread, never dialog widgets."""
+    result_ready = pyqtSignal(int, object, object)
+
+    def __init__(self, request_id, work):
+        super().__init__()
+        self.request_id = request_id
+        self.work = work
+
+    def run(self):
+        try:
+            result = self.work()
+        except Exception as exc:
+            logger.exception("Lyrics download dialog request failed")
+            self.result_ready.emit(self.request_id, None, str(exc))
+        else:
+            self.result_ready.emit(self.request_id, result, None)
 
 
 class LyricsDownloadDialog(QDialog, Ui_LyricsDownloadDialog):
     download_lrc_signal = pyqtSignal(object)
 
-    _load_data_signal = pyqtSignal(list)
-    _load_detail_signal = pyqtSignal(tuple)
-
     def __init__(self, parent=None):
         super(LyricsDownloadDialog, self).__init__(parent)
+        self._request_id = 0
         self.setupUi(self)
         self._init_table_widget()
         self._init_signal()
@@ -46,9 +68,6 @@ class LyricsDownloadDialog(QDialog, Ui_LyricsDownloadDialog):
         self.search_button.clicked.connect(self.search_event)
         self.download_button.clicked.connect(self.download_event)
         self.cancel_button.clicked.connect(self.close)
-        # 设置文本信号连接（为了便于线程设置文本不出现未响应）
-        self._load_detail_signal.connect(self._set_detail_label)
-        self._load_data_signal.connect(self._load_result_table_widget)
 
     def _init_api(self):
         """初始化使用的歌词api"""
@@ -102,141 +121,146 @@ class LyricsDownloadDialog(QDialog, Ui_LyricsDownloadDialog):
             self.search_tableWidget.setHorizontalHeaderItem(column, item)
         self.search_tableWidget.hideColumn(4)
 
-    @thread_drive()
     def search_event(self, *_):
-        """搜索事件"""
-        self.search_tableWidget.clearContents()
-
-        self.image_label.setPixmap(QPixmap())
-        self._load_detail_signal.emit((self.tr("正在搜索相关歌词，请稍后"), ""))
-
+        """在主线程读取输入和更新界面，只将网络查询交给后台。"""
         keyword = self.search_lineEdit.text()
+        self.search_tableWidget.setRowCount(0)
+        self.image_label.clear()
+        self._set_detail_label((self.tr("正在搜索相关歌词，请稍后"), ""))
+        self._set_controls_enabled(False)
+        self.warning_dialog.hide()
+        apis = {"spotify": self.spotify_api, "kugou": self.kugou_api, "cloud": self.cloud_api}
+        self._start_request(partial(self._search_sources, keyword, apis), self._search_done)
 
-        network_error = False
-
-        try:
-            res_spotify = self.spotify_api.search_song_id(keyword)
-        except NoneResultError:
-            res_spotify = []
-        except NetworkError:
-            res_spotify = []
-            self._load_detail_signal.emit((self.tr("连接到spotify网络错误"), ""))
-            network_error = True
-
-        try:
-            res_kugou = self.kugou_api.search_song_id(keyword)
-        except NoneResultError:
-            res_kugou = []
-        except NetworkError:
-            res_kugou = []
-            self._load_detail_signal.emit((self.tr("连接到kugou网络错误"), ""))
-            network_error = True
-
-        try:
-            res_cloud = self.cloud_api.search_song_id(keyword)
-        except NoneResultError:
-            res_cloud = []
-        except NetworkError:
-            res_cloud = []
-            self._load_detail_signal.emit((self.tr("连接到网易云网络错误"), ""))
-            network_error = True
-
-        # 交叉合并搜索结果，在前的优先级高
-        res_list = []
-        for i in range(len(res_kugou) + len(res_cloud) + len(res_spotify)):
-            if res_kugou:
-                data = res_kugou.pop(0)
-                new_data = (data.songName, data.singer, data.duration, "kugou", data.idOrMd5)
-                res_list.append(new_data)
-            if res_cloud:
-                data = res_cloud.pop(0)
-                new_data = (data.songName, data.singer, data.duration, "cloud", data.idOrMd5)
-                res_list.append(new_data)
-            if res_spotify:
-                data = res_spotify.pop(0)
-                new_data = (data.songName, data.singer, data.duration, "spotify", data.idOrMd5)
-                res_list.append(new_data)
-
-        if not res_list and not network_error:
-            self._load_detail_signal.emit((self.tr("搜索词无结果"), ""))
-            return
-
-        self.search_tableWidget.setRowCount(len(res_list))
-        self._load_data_signal.emit(res_list)
-        self._load_detail_signal.emit((self.tr("请在上方选择"), ""))
-
-    @thread_drive()
-    def result_click_event(self, item):
-        """点击搜索结果事件"""
-        self.search_tableWidget.setEnabled(False)
-        title = self.search_tableWidget.item(item.row(), 0).text()
-        singer = self.search_tableWidget.item(item.row(), 1).text()
-        api = self.search_tableWidget.item(item.row(), 3).text()
-        track_id = self.search_tableWidget.item(item.row(), 4).text()
-        if api == "kugou":
-            data_api = self.kugou_api
-        elif api == "spotify":
-            data_api = self.spotify_api
-        else:
-            data_api = self.cloud_api
-
-        image = self.temp_file_manage.get_temp_image(track_id)
-        self._load_detail_signal.emit((title, singer))
-
-        if not image.getvalue():
-            self.image_label.clear()
-            # self.image_label.setText("正在获取封面") 引发崩溃
+    @staticmethod
+    def _search_sources(keyword, apis):
+        results, errors = {}, []
+        for name, api in apis.items():
             try:
-                song_data = data_api.search_song_info(track_id, download_pic=True, pic_size=64)
-            except NetworkError:
-                self._load_detail_signal.emit((self.tr("网络错误，获取失败"), ""))
-                self.search_tableWidget.setEnabled(True)
-                return
-            image = song_data.picBuffer
-            if image:
+                results[name] = api.search_song_id(keyword)
+            except NoneResultError:
+                results[name] = []
+            except (NetworkError, UserError) as exc:
+                results[name] = []
+                errors.append(f"{name}: {exc}")
+        rows = []
+        sources = ("kugou", "cloud", "spotify")
+        for group in zip_longest(*(results[name] for name in sources)):
+            for name, data in zip(sources, group):
+                if data is not None:
+                    rows.append((data.songName, data.singer, data.duration, name, data.idOrMd5))
+        return rows, errors
+
+    @pyqtSlot(int, object, object)
+    def _search_done(self, request_id, result, error):
+        if request_id != self._request_id:
+            return
+        self._set_controls_enabled(True)
+        if error is not None:
+            self._set_detail_label((self.tr("搜索失败"), error))
+            return
+        rows, errors = result
+        self._load_result_table_widget(rows)
+        self.download_button.setEnabled(bool(rows))
+        message = self.tr("请在上方选择") if rows else self.tr("搜索词无结果")
+        self._set_detail_label((message, "; ".join(errors)))
+
+    def result_click_event(self, item):
+        """主线程快照选中行，旧封面请求不能覆盖新选择。"""
+        row = item.row()
+        title = self.search_tableWidget.item(row, 0).text()
+        singer = self.search_tableWidget.item(row, 1).text()
+        source = self.search_tableWidget.item(row, 3).text()
+        track_id = self.search_tableWidget.item(row, 4).text()
+        self._set_detail_label((title, singer))
+        self.image_label.clear()
+        self._request_id += 1
+        try:
+            image = self.temp_file_manage.get_temp_image(track_id)
+        except OSError:
+            logger.exception("Cannot read cover cache")
+            image = None
+        if image and image.getvalue():
+            self._show_image(image.getvalue())
+            return
+        self._start_request(partial(self._fetch_cover, self._source_api(source), track_id), self._cover_done)
+
+    @staticmethod
+    def _fetch_cover(api, track_id):
+        data = api.search_song_info(track_id, download_pic=True, pic_size=64)
+        return track_id, data.picBuffer
+
+    @pyqtSlot(int, object, object)
+    def _cover_done(self, request_id, result, error):
+        if request_id != self._request_id:
+            return
+        if error is not None:
+            self.image_label.setText(self.tr("封面获取失败"))
+            return
+        track_id, image = result
+        if image and image.getvalue():
+            try:
                 self.temp_file_manage.save_temp_image(track_id, image)
-            else:
-                image = io.BytesIO()
+            except OSError:
+                logger.exception("Cannot save cover cache")
+            self._show_image(image.getvalue())
 
-        item = self.search_tableWidget.currentItem()
+    def _show_image(self, data):
+        pix = QPixmap()
+        pix.loadFromData(data)
+        self.image_label.setPixmap(pix)
 
-        if item and item.row() == item.row():
-            pix = QPixmap()
-            if image.getvalue():
-                pix.loadFromData(image.read())
-                self.image_label.setPixmap(pix)
-            else:
-                # self.image_label.setText("无图片")
-                ...
-        self.search_tableWidget.setEnabled(True)
+    def _source_api(self, source):
+        return {"kugou": self.kugou_api, "spotify": self.spotify_api, "cloud": self.cloud_api}[source]
 
     def download_event(self, item=None):
         """下载歌词事件"""
-        if not item:
+        if item is None or isinstance(item, bool):
             item = self.search_tableWidget.currentItem()
 
         if not item:
             return
-        self.search_tableWidget.setEnabled(False)
-
         track_id_or_md5 = self.search_tableWidget.item(item.row(), 4).text()
         api = self.search_tableWidget.item(item.row(), 3).text()
-        if api == "kugou":
-            data_api = self.kugou_api
-        elif api == "spotify":
-            data_api = self.spotify_api
-        else:
-            data_api = self.cloud_api
-        try:
-            lrc = data_api.fetch_song_lyric(track_id_or_md5)
-        except (NetworkError, NoneResultError, UserError) as e:
-            self.warning_dialog.show()
-            self.warning_dialog.set_text(str(e))
-            self.search_tableWidget.setEnabled(True)
+        self._set_controls_enabled(False)
+        self.warning_dialog.hide()
+        self._start_request(partial(self._source_api(api).fetch_song_lyric, track_id_or_md5), self._download_done)
+
+    @pyqtSlot(int, object, object)
+    def _download_done(self, request_id, lrc, error):
+        if request_id != self._request_id:
             return
-        self.search_tableWidget.setEnabled(True)
+        self._set_controls_enabled(True)
+        if error is not None:
+            self.warning_dialog.set_text(error)
+            self.warning_dialog.show()
+            return
         self.download_lrc_signal.emit(lrc)
         self.close()
+
+    def _start_request(self, work, callback):
+        self._request_id += 1
+        task = _DownloadTask(self._request_id, work)
+        task.result_ready.connect(callback, Qt.ConnectionType.QueuedConnection)
+        thread_manager.add_thread(task)
+        task.start()
+
+    def _set_controls_enabled(self, enabled):
+        self.search_tableWidget.setEnabled(enabled)
+        self.search_button.setEnabled(enabled)
+        self.search_lineEdit.setEnabled(enabled)
+        self.download_button.setEnabled(enabled and self.search_tableWidget.rowCount() > 0)
+
+    def hideEvent(self, event):
+        # Closing settings may hide/delete this dialog while a request is in
+        # flight. Queued QObject slots auto-disconnect on deletion; a reusable
+        # hidden dialog additionally invalidates results from its old session.
+        self._request_id += 1
+        super().hideEvent(event)
+
+    def showEvent(self, event):
+        self._set_controls_enabled(True)
+        super().showEvent(event)
 
     def _set_detail_label(self, texts):
         """设置文本，利于非主线程控制"""
@@ -245,8 +269,9 @@ class LyricsDownloadDialog(QDialog, Ui_LyricsDownloadDialog):
 
     def _load_result_table_widget(self, result_data):
         """设置搜索结果到表格，利于非主线程控制"""
+        self.search_tableWidget.setRowCount(len(result_data))
         for outer_index, outer_data in enumerate(result_data):
             for inner_index, inner_data in enumerate(outer_data):
                 item = QTableWidgetItem()
-                item.setText(inner_data)
+                item.setText(str(inner_data))
                 self.search_tableWidget.setItem(outer_index, inner_index, item)
