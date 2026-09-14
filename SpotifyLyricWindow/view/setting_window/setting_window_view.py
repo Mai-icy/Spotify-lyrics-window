e9072e13905d3cfe5c3bc164411e0508dae6883f
Maiicy <Maiicy@foxmail.com>
2026-09-14 16:30:02 +0800

    :lipstick: integrate refreshed settings pages and sidebar navigation

--- SpotifyLyricWindow/view/setting_window/setting_window_view.py
+++ SpotifyLyricWindow/view/setting_window/setting_window_view.py
@@ -6,16 +6,14 @@ from PyQt6.QtCore import *
 from PyQt6.QtGui import *
 from PyQt6.QtWidgets import *
 
-from common.path import QSS_PATH
 from common.config import Config
 from components.widget.mask_widget import MaskWidget
-from components.raw_ui import Ui_SettingsWindow
+from components.settings_ui import Ui_SettingsWindow, ASSET_PATH, scroll_page
 from view.setting_page.lyric_setting_page import LyricPage
 from view.setting_page.hotkey_setting_page import HotkeysPage
 from view.setting_page.common_setting_page import CommonPage
 from view.setting_page.lyrics_manage_page import LyricsManagePage
 
-import view.setting_window.lightstyle_rc
 
 class SettingWindow(QWidget, Ui_SettingsWindow):
     close_signal = pyqtSignal(object)
@@ -29,6 +27,7 @@ class SettingWindow(QWidget, Ui_SettingsWindow):
         self._init_page()
         self._init_signal()
         self._init_style_sheet()
+        self.page_listWidget.setCurrentRow(0)
 
     def _init_page(self):
         """初始化设置页面"""
@@ -39,24 +38,37 @@ class SettingWindow(QWidget, Ui_SettingsWindow):
         self.hotkeys_page = HotkeysPage(self)
         self.lyric_manage_page = LyricsManagePage(setting_window=self)
 
-        self.page_stackedWidget.addWidget(self.common_page)
-        self.page_stackedWidget.addWidget(self.hotkeys_page)
-        self.page_stackedWidget.addWidget(self.lyric_page)
+        self.page_stackedWidget.addWidget(scroll_page(self.common_page))
+        self.page_stackedWidget.addWidget(scroll_page(self.hotkeys_page))
+        self.page_stackedWidget.addWidget(scroll_page(self.lyric_page))
         self.page_stackedWidget.addWidget(self.lyric_manage_page)
 
     def _init_signal(self):
         """初始化信号"""
-        self.page_listWidget.itemClicked.connect(self.page_click_event)
+        self.page_listWidget.currentRowChanged.connect(self.page_change_event)
 
     def _init_style_sheet(self):
-        with QSS_PATH.open("r") as f:
-            style_sheet = f.read()
+        with (ASSET_PATH / 'settings.qss').open(encoding='utf-8') as f:
+            style_sheet = f.read().replace('@assets', ASSET_PATH.as_posix())
         self.setStyleSheet(style_sheet)
+
+    def page_change_event(self, index):
+        if not 0 <= index < self.page_stackedWidget.count():
+            return
+        descriptions = (
+            self.tr('管理窗口行为、歌词同步和 Spotify 连接。'),
+            self.tr('不用离开当前应用，也能轻松控制歌词。'),
+            self.tr('调整方向、字体与配色，找到舒服的阅读方式。'),
+            self.tr('浏览、编辑与整理已经下载的歌词。'),
+        )
+        self.page_stackedWidget.setCurrentIndex(index)
+        self.page_title.setText(self.page_listWidget.item(index).text())
+        self.page_description.setText(descriptions[index])
 
     def page_click_event(self, item: QListWidgetItem):
         """切换配置页面"""
         index = self.page_listWidget.row(item)
-        self.page_stackedWidget.setCurrentIndex(index)
+        self.page_listWidget.setCurrentRow(index)
 
     def set_always_front(self, flag: bool):
         """
